cfc37bf7ce7d771a2880ea39deeb28e1ae8291b7
lifeng <lifeng@ipfsforce.com>
2022-04-16 17:32:47 +0800

    chore:extract mutcask from the repo of filedag-storage

---
+++ interface.go
@@ -0,0 +1,19 @@
+package mutcask
+
+import (
+	"context"
+
+	"golang.org/x/xerrors"
+)
+
+var ErrNotFound = xerrors.New("kv: key not found")
+
+type KVDB interface {
+	Put(string, []byte) error
+	Delete(string) error
+	Get(string) ([]byte, error)
+	Size(string) (int, error)
+
+	AllKeysChan(context.Context) (chan string, error)
+	Close() error
+}
